e4e698a3972856a48e4d990867c7b8cec0c61a4d
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2022-03-10 15:11:10 +0100

    newsubject

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lle-briq <lle-briq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/24 07:23:49 by lle-briq          #+#    #+#             */
-/*   Updated: 2021/12/24 07:34:36 by lle-briq         ###   ########.fr       */
+/*   Updated: 2022/03/10 15:10:50 by lle-briq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	testBasic(void)
 {
-	Bureaucrat	bob("bob", 3);
+	Bureaucrat	bob("bob", 6);
 
 	std::cout << bob << std::endl;
 	bob.downGrade();
@@ -36,11 +36,36 @@ void	testExceptionHigh(void)
 	Bureaucrat	bob("bob", 0);
 }
 
+void	testDownGrade(void)
+{
+	Bureaucrat	bob("bob", 2);
+	bob.upGrade();
+	std::cout << bob << std::endl;
+	bob.upGrade();
+}
+
+void	testUpGrade(void)
+{
+	Bureaucrat	bob("bob", 149);
+	bob.downGrade();
+	std::cout << bob << std::endl;
+	bob.downGrade();
+}
+
 int	main(void)
 {
 	try
 	{
 		testBasic();
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	std::cout << std::endl;
+	try
+	{
 		testExceptionLow();
 	}
 	catch (std::exception &e)
@@ -48,9 +73,30 @@ int	main(void)
 		std::cerr << e.what() << std::endl;
 	}
 
+	std::cout << std::endl;
 	try
 	{
 		testExceptionHigh();
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
+	std::cout << std::endl;
+	try
+	{
+		testDownGrade();
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	std::cout << std::endl;
+	try
+	{
+		testUpGrade();
 	}
 	catch (std::exception &e)
 	{
